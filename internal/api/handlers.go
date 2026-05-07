6416d81706bb8c5c9f46ed865948bb1421f357d6
DXICIDE <Rostaknc@seznam.cz>
2026-05-07 17:06:02 +0200

    FEAT: handler for the lookup function

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,1 +1,59 @@
 package api
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/DXICIDE/MagisterLinguae/internal/database"
+)
+
+type WordResponse struct {
+	Word       string    `json:"word"`
+	Known      bool      `json:"known"`
+	Frequency  int       `json:"frequency"`
+	LastSeenAt time.Time `json:"last_seen_at"`
+}
+
+type Handler struct {
+	db *database.Queries
+}
+
+func (h *Handler) GetWord(w http.ResponseWriter, r *http.Request) {
+	word := r.PathValue("word")
+	languageID := r.URL.Query().Get("language_id")
+
+	ID, err := strconv.Atoi(languageID)
+	if err != nil {
+		http.Error(w, "invalid language_id", http.StatusBadRequest)
+		return
+	}
+
+	wordDB, err := h.db.GetWord(r.Context(), database.GetWordParams{
+		TokenName:  word,
+		LanguageID: int32(ID),
+	})
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			http.Error(w, "word not found", http.StatusNotFound)
+			return
+		}
+		log.Printf("GetWord error: %v", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(WordResponse{
+		Word:       wordDB.TokenName,
+		Known:      wordDB.Known,
+		Frequency:  int(wordDB.Frequency),
+		LastSeenAt: wordDB.LastSeenAt,
+	})
+
+}
