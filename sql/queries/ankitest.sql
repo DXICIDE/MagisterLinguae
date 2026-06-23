c0523af8825cb88ef5843d4d7a0d98dbd5e24961
DXICIDE <Rostaknc@seznam.cz>
2026-06-23 22:46:29 +0200

    FEAT: modified anki

--- sql/queries/ankitest.sql
+++ sql/queries/ankitest.sql
@@ -1,5 +1,5 @@
 -- name: GetAnki :many
 SELECT * FROM words
-WHERE frequency > 1 AND language_id = $1 AND known = true AND last_seen_at < NOW() - INTERVAL '7 days'
+WHERE frequency > 2 AND language_id = $1 AND known = true AND last_seen_at < NOW() - INTERVAL '7 days'
 ORDER BY last_seen_at ASC 
 LIMIT $2;
